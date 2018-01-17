1108f637a6f48ef9e53a054e46c0aa22bbb1754d
Logi Leifsson <logi@wuxinextcode.com>
2018-01-17 20:48:22 +0000

    added tests

---
+++ test/test_main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from contextlib import contextmanager
+import unittest
+import mock
+import json
+import sys
+
+# StringIO module is not available in python3
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from shellson.__main__ import main
+
+
+# Context manager function to capture stdout
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+def mock_read_stdin():
+    return json.dumps({'param1': 'value1'})
+
+
+class TestMain(unittest.TestCase):
+    """unit tests for shellson __main__.py"""
+
+    @mock.patch('shellson.__main__.read_stdin', new=mock_read_stdin)
+    def test_get(self):
+        with captured_output() as (out, err):
+            main('get', 'param1')
+            output = out.getvalue().strip()
+        assert json.loads(output) == 'value1'
